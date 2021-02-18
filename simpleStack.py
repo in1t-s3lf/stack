93cabd0eeba719ef219532e6aa6c2b3256be1eae
in1t-s3lf <36043458+in1t-s3lf@users.noreply.github.com>
2021-02-18 19:07:42 +0300

    Update simpleStack.py

--- simpleStack.py
+++ simpleStack.py
@@ -7,10 +7,12 @@ while(ext==False):
     if(cmd[:4]=='push'):
         stack.append(cmd[5:])
         print('ok')
-    elif(cmd=='pop'):
-        print(stack.pop())
-    elif(cmd=='back'):
-        print(stack[len(stack)-1])
+    elif(cmd=='pop' or cmd=='back'):
+        if(len(stack)>=1 and cmd=='pop'):
+            print(stack.pop())
+        elif(len(stack)>=1 and cmd=='back'):
+            print(stack[len(stack)-1])
+        else: print('error')
     elif(cmd=='size'):
         print(len(stack))
     elif(cmd=='clear'):
